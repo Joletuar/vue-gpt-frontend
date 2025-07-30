c676823623ba813eff0e21cf07fb42a4fff5aea7
Johan <joletuar.dev@gmail.com>
2025-07-30 07:26:56 -0500

    feat: add typing loader component
    - Create TypingLoader with animated dots indicating message processing
    - Add CSS animations for scaling and bouncing effects
    - Include customizable timing and visual styling for loading states

---
+++ src/presentation/components/loaders/TypingLoader.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="typing" v-bind="$attrs">
+    <span class="circle scaling"></span>
+    <span class="circle scaling"></span>
+    <span class="circle scaling"></span>
+  </div>
+</template>
+
+<style scoped src="./TypingLoader.css"></style>
